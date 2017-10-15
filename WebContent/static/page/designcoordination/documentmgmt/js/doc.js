2cc68cf08d5801fe935b916700b94ffb9f0d266d
wx <wx@DESKTOP-GTKDICE.mshome.net>
2017-10-15 21:56:08 +0800

    添加了预览文档的方法

--- WebContent/static/page/designcoordination/documentmgmt/js/doc.js
+++ WebContent/static/page/designcoordination/documentmgmt/js/doc.js
@@ -26,6 +26,7 @@ $(function () {
                 		$("tbody tr").remove();
                 		$("tbody").append(treeNode.doc);
                 		todownload();
+                		toyulan();
                 	}
                 }
             };
@@ -34,6 +35,7 @@ $(function () {
             var node = zTreeObj.getNodeByParam("id", 1, null);
             zTreeObj.selectNode(node);
             todownload();
+            toyulan();
         }
 	    
     });
@@ -54,3 +56,29 @@ function todownload(){
      	})
      });
 }
+function toyulan(){
+	$(".yl").each(function(){
+     	$(this).click(function(){
+     		console.log(this);
+     		var filename = $(this).parents("tr").children().eq(0).text();
+     		var rootPath = getRootPath();
+     		window.open(rootPath+"/static/page/designcoordination/documentmgmt/wendang/"+filename);
+     	})
+     });
+}
+/** 
+ * //获取当前项目根路径 
+ * @return {TypeName}  
+ */  
+function getRootPath(){      
+    //获取当前网址，如： http://localhost:8083/uimcardprj/share/meun.jsp      
+    var curWwwPath=window.document.location.href;      
+    //获取主机地址之后的目录，如： uimcardprj/share/meun.jsp      
+    var pathName=window.document.location.pathname;      
+    var pos=curWwwPath.indexOf(pathName);      
+    //获取主机地址，如： http://localhost:8083      
+    var localhostPaht=curWwwPath.substring(0,pos);      
+    //获取带"/"的项目名，如：/uimcardprj      
+    var projectName=pathName.substring(0,pathName.substr(1).indexOf('/')+1);      
+    return(localhostPaht+projectName);  
+}  
